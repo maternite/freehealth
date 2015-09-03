fec3b95a748b85a62b8fc8f7221098efc26f9312
Jerome Pinguet <jerome@jerome.cc>
2015-09-03 15:12:51 +0300

    change directory structure

---
+++ forms/subforms/generic/soap_test/scripts/xx/script.js
@@ -0,0 +1,58 @@
+namespace.module('com.freemedforms.generic.soapWithBio', function (exports, require) {
+
+    exports.extend({
+                       'setupUi': setupUi,
+                       'computePulsePressure': computePulsePressure
+                   });
+
+    // Ui vars (retrieved from the ui)
+    var syst, diast, pulse;
+
+    function setupUi() {
+        print("com.freemedforms.generic.soapWithBio Setup UI");
+
+        // Get items to work with
+        freemedforms.forms.namespaceInUse = "";
+        var formItem = freemedforms.forms.item("Subs::Tools::SOAP::WithBio");
+        print(formItem);
+        formUi = formItem.ui();
+        syst = formUi.findChild("bpSyst");
+        diast = formUi.findChild("bpDiast");
+        pulse = formUi.findChild("bpPulse");
+        groupap = formUi.findChild("apGroup");
+        groupo = formUi.findChild("oGroup");
+
+//      populateCombos();
+        
+        // Connect data changed on spins
+        // Spin should use valueChanged(int)
+        // DoubleSpin should use valueChanged(double)
+
+        syst['valueChanged(int)'].connect(this, computePulsePressure);          
+        diast['valueChanged(int)'].connect(this, computePulsePressure);
+
+        // hide apGroup
+        groupap.visible = false;
+        groupo.visible = false;
+
+    }
+
+//     function populateCombos() {                                                 
+       // TODO: add weight and height units                                    
+//        freemedforms.uiTools.addItems(cholCombo, totalCholRanges);            
+//        freemedforms.uiTools.addItems(hdlCombo, hdlCholRanges);               
+//        freemedforms.uiTools.addItems(systCombo, systolicRanges);             
+//    } 
+
+    function retranslateUi() {
+        var lang = freemedforms.forms.currentLanguage;
+    }
+
+    function computePulsePressure() {
+        print("Compute pulse pressure.");
+        var text = syst.value - diast.value;                                    
+        pulse.setText(text);                                                    
+    }
+});
+
+namespace.com.freemedforms.generic.soapWithBio.setupUi();
