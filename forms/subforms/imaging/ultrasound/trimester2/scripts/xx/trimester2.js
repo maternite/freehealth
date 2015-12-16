c5dbcd77641fdb92f11b0e76e3aabc8afc591283
Jerome Pinguet <jerome@jerome.cc>
2015-12-16 04:39:35 +0200

    improve printing & scripting for US 2nd trimester

--- forms/subforms/imaging/ultrasound/trimester2/scripts/xx/trimester2.js
+++ forms/subforms/imaging/ultrasound/trimester2/scripts/xx/trimester2.js
@@ -9,11 +9,19 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
         'trimester2_computeGestationalAge': trimester2_computeGestationalAge,
         'trimester2_setLmp': trimester2_setLmp,
         'trimester2_setDateOfConception': trimester2_setDateOfConception,
-        'trimester2_setLmpDefault': trimester2_setLmpDefault
+        'trimester2_setLmpDefault': trimester2_setLmpDefault,
+        'hideCalendarLineEdit': hideCalendarLineEdit
     });
 
     var trimester2_fetusNumber, trimester2_fetusGroup1, trimester2_fetusGroup2, trimester2_fetusGroup3, trimester2_fetusGroup4, trimester2_fetusGroup5, trimester2_fetusGroup6;
-    var trimester2_gestationalAge, trimester2_dateToUse, trimester2_estimatedDueDate, trimester2_lastMenstrualPeriodUi, trimester2_lastMenstrualPeriodItem, trimester2_conceptionDateUi, trimester2_conceptionDateItem, trimester2_useLmp, trimester2_useLmpItem, trimester2_useDateOfConception, trimester2_useDateOfConceptionItem;
+    var trimester2_gestationalAge, trimester2_dateToUse, trimester2_estimatedDueDate, trimester2_lastMenstrualPeriodUi, trimester2_lastMenstrualPeriodItem, trimester2_conceptionDateUi, trimester2_conceptionDateItem, trimester2_useLmp, trimester2_useLmpItem, trimester2_useDateOfConception, trimester2_useDateOfConceptionItem, trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit, trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit, defaultDate;
+
+    (function () {
+    var x = "Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    print(x);      // I will invoke myself
+    trimester2_setupUi();
+    hideCalendarLineEdit();
+    })();
 
     function trimester2_setupUi() {
 
@@ -36,17 +44,21 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
         trimester2_defaultHideFetusGroupBox();
 
         // obstetrical calendar
+        defaultDate = "01011900"
         trimester2_lastMenstrualPeriodUi = trimester2_formUi.findChild("lastMenstrualPeriodDateEdit");
-        trimester2_lastMenstrualPeriodItem = freemedforms.forms.item("Trimester2::ObstetricalCalendar::LastMenstrualPeriod");
+        trimester2_lastMenstrualPeriodItem = freemedforms.forms.item("Maternity::Imaging::Ultrasound::Trimester2::Trimester2::ObstetricalCalendar::LastMenstrualPeriod");
         trimester2_conceptionDateUi = trimester2_formUi.findChild("conceptionDateDateEdit");
-        trimester2_conceptionDateItem = freemedforms.forms.item("Trimester2::ObstetricalCalendar::ConceptionDate");
+        trimester2_conceptionDateItem = freemedforms.forms.item("Maternity::Imaging::Ultrasound::Trimester2::Trimester2::ObstetricalCalendar::ConceptionDate");
         trimester2_useLmp = trimester2_formUi.findChild("useLmpCheck");
-        trimester2_useLmpItem = freemedforms.forms.item("Trimester2::ObstetricalCalendar::UseLMP::Check");
+        trimester2_useLmpItem = freemedforms.forms.item("Maternity::Imaging::Ultrasound::Trimester2::ObstetricalCalendar::UseLMP::Check");
         trimester2_useDateOfConception = trimester2_formUi.findChild("useDateOfConceptionCheck");
-        trimester2_useDateOfConceptionItem = freemedforms.forms.item("Trimester2::ObstetricalCalendar::UseConceptionDate::Check");        
-        trimester2_currentWeeksAmenorrhoeaWeeksValue = trimester2_formUi.findChild("trimester2CurrentWeeksAmenorrhoeaWeeksValueLabel");        
+        trimester2_useDateOfConceptionItem = freemedforms.forms.item("Maternity::Imaging::Ultrasound::Trimester2::ObstetricalCalendar::UseConceptionDate::Check");        
+        trimester2_currentWeeksAmenorrhoeaWeeksValue = trimester2_formUi.findChild("trimester2CurrentWeeksAmenorrhoeaWeeksValueLabel");
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit = trimester2_formUi.findChild("trimester2CurrentWeeksAmenorrhoeaWeeksValueLineEdit");
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit = trimester2_formUi.findChild("trimester2CurrentWeeksAmenorrhoeaDaysValueLineEdit");  
         trimester2_currentWeeksAmenorrhoeaDaysValue = trimester2_formUi.findChild("trimester2CurrentWeeksAmenorrhoeaDaysValueLabel");       
         trimester2_estimatedDueDate = trimester2_formUi.findChild("eddValueLabel");
+        trimester2_estimatedDueDateLineEdit = trimester2_formUi.findChild("eddValueLineEdit");
         trimester2_setDefaults();
         trimester2_setLmpDefault();
 
@@ -59,8 +71,19 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
         //lastMenstrualPeriod['dateChanged(int)'].connect(this, chooseDateToUse);  
         //conceptionDate['dateChanged()'].connect(this, chooseDateToUse);
 
+        // hide LineEdit widgets used to print and record dynamic data:
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit.visible = false;   
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit.visible = false;
+        trimester2_estimatedDueDateLineEdit.visible = false;
     }
     
+    function hideCalendarLineEdit() {
+        // hide LineEdit widgets used to print and record dynamic data:         
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit.visible = false;   
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit.visible = false;    
+        trimester2_estimatedDueDateLineEdit.visible = false;
+    }
+
     // fetus functions
 
     function trimester2_defaultHideFetusGroupBox() {
@@ -146,14 +169,36 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
 
     // obstetrical calendar functions
 
+    function hideCalendarLineEdit() {                                           
+        // hide LineEdit widgets used to print and record dynamic data:         
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit.visible = false;   
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit.visible = false;    
+        trimester2_estimatedDueDateLineEdit.visible = false;                    
+    }
+
+    function isDateDefault(date) {
+        
+    }
+
+    function trimester2_emptyEdd() {
+        var trimester2_empty = "";                                              
+        trimester2_estimatedDueDate.setText(trimester2_empty);
+    }
+
+    function trimester2_emptyGa() {
+        var trimester2_emptyGA = "";                                            
+        trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_emptyGA);
+        trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_emptyGA);
+    }
+
     function trimester2_chooseDateToUse() {
         print("trimester2_chooseDateToUse()");
-        var trimester2_empty = "";
-        trimester2_estimatedDueDate.setText(trimester2_empty);
+        trimester2_emptyEdd();
 
         if (!trimester2_useLmpItem.checked && !trimester2_useDateOfConceptionItem.checked) {
         trimester2_setLmpDefault();
-        return;
+        trimester2_computeGestationalAge();
+        trimester2_computeEstimatedDueDate()
         }
         trimester2_computeEstimatedDueDate();
         trimester2_computeGestationalAge();
@@ -161,33 +206,68 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
     }
 
     function trimester2_computeEstimatedDueDate() {
-        var trimester2_empty = "";
-        trimester2_estimatedDueDate.setText(trimester2_empty);
-        trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_empty);                     
-        trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_empty);
-        if (freemedforms.tools.dateToString(trimester2_lastMenstrualPeriodItem.currentValue, "ddd dd MMM yyyy") == "Sat 01 Jan 2000") {
-            print("inside trimester2_computeEstimatedDueDate()-> lmp=01/01/2000 -> return");
+        hideCalendarLineEdit();
+        trimester2_emptyEdd();        
+        //trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_empty);                     
+        //trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_empty);
+        if (trimester2_useLmpItem.checked) {
+            trimester2_computeEddFromLmp();
+        } else {
+            trimester2_computeEddFromCd();
+        }
+    }
+
+    function trimester2_computeEddFromLmp() {
+
+        // If date is defaultDate, do nothing
+        if (freemedforms.tools.dateToString(trimester2_lastMenstrualPeriodItem.currentValue, "ddMMyyyy") == defaultDate) {
+            print("inside trimester2_computeEstimatedDueDate()-> lmp=01/01/1900 -> return");
             return;
         }
-        print("trimester2_computeEstimatedDueDate()");
+        print("trimester2_computeEddFromLmp()");
         var trimester2_edd = freemedforms.tools.addWeeks(trimester2_lastMenstrualPeriodItem.currentValue, 40);                   
         var trimester2_edd = freemedforms.tools.dateToString(trimester2_edd, "ddd dd MMM yyyy");
         var trimester2_eddText = "<b>";
         trimester2_eddText += trimester2_edd;
         trimester2_eddText += "</b>";
         trimester2_estimatedDueDate.setText(trimester2_eddText);        
+        trimester2_estimatedDueDateLineEdit.setText(trimester2_eddText);
+    }
 
+    function trimester2_computeEddFromCd() {                                   
+                                                                                
+        // If date is defaultDate, do nothing                                   
+        if (freemedforms.tools.dateToString(trimester2_conceptionDateItem.currentValue, "ddMMyyyy") == defaultDate) {
+            print("inside trimester2_computeEddFromCd()-> cd=01/01/1900 -> return");
+            trimester2_emptyEdd()
+            return;                                                             
+        }                                                                       
+        print("trimester2_computeEddFromCd()");                                
+        var trimester2_edd = freemedforms.tools.addDays(trimester2_conceptionDateItem.currentValue, 266);
+        var trimester2_edd = freemedforms.tools.dateToString(trimester2_edd, "ddd dd MMM yyyy");
+        var trimester2_eddText = "<b>";                                         
+        trimester2_eddText += trimester2_edd;                                   
+        trimester2_eddText += "</b>";                                           
+        trimester2_estimatedDueDate.setText(trimester2_eddText);                
+        trimester2_estimatedDueDateLineEdit.setText(trimester2_eddText);        
     }
 
     function trimester2_computeGestationalAge() {
-        var trimester2_emptyGA = "";
-        trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_emptyGA);                     
-        trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_emptyGA);
-        if (freemedforms.tools.dateToString(trimester2_lastMenstrualPeriodItem.currentValue, "ddd dd MMM yyyy") == "Sat 01 Jan 2000") {
-            print("inside trimester2_computeGestationalAge()-> lmp=01/01/2000 -> return");
-            return;                                                             
-        }                                        
-        print("computeGestationalAge()");
+        hideCalendarLineEdit();
+        trimester2_emptyGa();    
+        
+        if (trimester2_useLmpItem.checked) {
+            trimester2_computeGaFromLmp();
+        } else {
+            trimester2_computeGaFromCd();
+        }
+    }
+    
+    function trimester2_computeGaFromLmp() {
+        if (freemedforms.tools.dateToString(trimester2_lastMenstrualPeriodItem.currentValue, "ddMMyyyy") == defaultDate) {
+            return;
+        }
+        print("computeGaFromLmp()");
         var trimester2_edd = freemedforms.tools.addWeeks(trimester2_lastMenstrualPeriodItem.currentValue, 40);
         var trimester2_days = freemedforms.tools.daysTo(new Date(), trimester2_edd);
         var trimester2_GAdays = (280 - trimester2_days);
@@ -201,7 +281,32 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
         trimester2_GAdaysText += "</b>";
         trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_GAweeksText);
         trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_GAdaysText);
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit.setText(trimester2_GAweeksText);
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit.setText(trimester2_GAdaysText);
     }
+
+    function trimester2_computeGaFromCd() {                                    
+        if (freemedforms.tools.dateToString(trimester2_conceptionDateItem.currentValue, "ddMMyyyy") == defaultDate) {
+            return;                                                             
+        }                                                                       
+        print("computeGaFromCd()");                                            
+        var trimester2_edd = freemedforms.tools.addDays(trimester2_conceptionDateItem.currentValue, 266);
+        var trimester2_days = freemedforms.tools.daysTo(new Date(), trimester2_edd);
+        var trimester2_GAdays = (280 - trimester2_days);                        
+        var trimester2_GAweeks = (trimester2_GAdays - trimester2_GAdays%7) / 7; 
+        var trimester2_GAdays = trimester2_GAdays%7;                            
+        var trimester2_GAweeksText = "<b>";                                     
+        trimester2_GAweeksText += trimester2_GAweeks;                           
+        trimester2_GAweeksText += "</b>";                                       
+        var trimester2_GAdaysText = "<b>";                                      
+        trimester2_GAdaysText += trimester2_GAdays;                             
+        trimester2_GAdaysText += "</b>";                                        
+        trimester2_currentWeeksAmenorrhoeaWeeksValue.setText(trimester2_GAweeksText);
+        trimester2_currentWeeksAmenorrhoeaDaysValue.setText(trimester2_GAdaysText);
+        trimester2_currentWeeksAmenorrhoeaWeeksValueLineEdit.setText(trimester2_GAweeksText);
+        trimester2_currentWeeksAmenorrhoeaDaysValueLineEdit.setText(trimester2_GAdaysText);
+    }
+
 
     function trimester2_setLmpDefault() {
         print("trimester2_setLmpDefault");
@@ -211,14 +316,18 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
 
     function trimester2_setDefaults() {                                                  
         print("trimester2_setDefaults");                                                 
-        trimester2_lastMenstrualPeriodItem.currentValue = (new Date());                    
+        //trimester2_lastMenstrualPeriod = (new Date());                    
         trimester2_estimatedDueDate.setText("");                                           
         return;                                                                 
     }
 
     function trimester2_setLmp() {
-        if (trimester2_useLmpItem.checked && !trimester2_useDateOfConceptionItem.checked) return;
-        
+        print("inside trimester2_setLmp()");
+        if (trimester2_useLmpItem.checked && !trimester2_useDateOfConception.checked) {
+            trimester2_computeEstimatedDueDate();                                   
+            trimester2_computeGestationalAge();
+            return;
+        }
         print("trimester2_setLmp()");
         
         if (trimester2_useLmpItem.checked && trimester2_useDateOfConceptionItem.checked) {
@@ -233,7 +342,13 @@ namespace.module('com.freemedforms.imaging.ultrasound.trimester2', function (exp
     }
 
     function trimester2_setDateOfConception() {
-        if (trimester2_useDateOfConception.checked && !trimester2_useLmpItem.checked) return;                                                         
+        trimester2_emptyEdd();
+        
+        //if (trimester2_useDateOfConception.checked && !trimester2_useLmpItem.checked) {
+        //trimester2_computeEstimatedDueDate();                               
+        //trimester2_computeGestationalAge();
+        //return;
+        //}                                                   
         if (trimester2_useLmpItem.checked && trimester2_useDateOfConceptionItem.checked) {
             trimester2_useLmpItem.checked = false;
         }
