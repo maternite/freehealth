91e213ff2f777f0f24f40eb60b0a89e1c3d4e4ec
Jerome Pinguet <jerome@jerome.cc>
2016-01-28 18:22:59 +0100

    gynecology constation

--- forms/subforms/obstetrics/prenatal/scripts/xx/prenatal.js
+++ forms/subforms/obstetrics/prenatal/scripts/xx/prenatal.js
@@ -12,14 +12,17 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
         'prenatal_computeGestationalAge': prenatal_computeGestationalAge,
         'prenatal_setLmp': prenatal_setLmp,
         'prenatal_setDateOfConception': prenatal_setDateOfConception,
-        'prenatal_setLmpDefault': prenatal_setLmpDefault
+        'prenatal_setLmpDefault': prenatal_setLmpDefault,
+        'prenatal_computeBMI': prenatal_computeBMI
     });
 
     var prenatal_fetusNumber, prenatal_fetusGroup1, prenatal_fetusGroup2, prenatal_fetusGroup3, prenatal_fetusGroup4, prenatal_fetusGroup5, prenatal_fetusGroup6;
     var prenatal_syst, prenatal_diast, prenatal_pulse;
-    var prenatal_weight, prenatal_weightPrePregnancy, prenatal_weightGain;
+    var prenatal_weight, prenatal_weightPrePregnancy, prenatal_weightGain, prenatal_weightGainLineEdit;
     var prenatal_fatigueScore, prenatal_fatigueScoreLineEdit, prenatal_standing, prenatal_industrial, prenatal_repetitive, prenatal_physical, prenatal_hardness;
     var prenatal_gestationalAge, prenatal_dateToUse, prenatal_estimatedDueDate, prenatal_lastMenstrualPeriodUi, prenatal_lastMenstrualPeriodItem, prenatal_conceptionDateUi, prenatal_conceptionDateItem, prenatal_useLmp, prenatal_useLmpItem, prenatal_useDateOfConception, prenatal_useDateOfConceptionItem, defaultDate;
+    // BMI variables
+    var prenatal_weightUnit, prenatal_weightUnitItem, prenatal_height, prenatal_heightUnit, prenatal_heightUnitItem, prenatal_bmiValueLabel, prenatal_bmiValueLineEdit;
 
     function prenatal_setupUi() {
 
@@ -50,6 +53,7 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
         prenatal_weight = prenatal_formUi.findChild("weightValue");
         prenatal_weightPrePregnancy = prenatal_formUi.findChild("prePregnancyWeightValue");
         prenatal_weightGain = prenatal_formUi.findChild("weightGainValue");
+        prenatal_weightGainLineEdit = prenatal_formUi.findChild("weightGainValueLineEdit");
 
         // fatigue score
         prenatal_standing = prenatal_formUi.findChild("standingCheck");
@@ -62,19 +66,29 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
         // obstetrical calendar
         defaultDate = "01011900";
         prenatal_lastMenstrualPeriodUi = prenatal_formUi.findChild("lastMenstrualPeriodDateEdit");
-        prenatal_lastMenstrualPeriodItem = freemedforms.forms.item("ObstetricalCalendar::LastMentrualPeriod");
+        prenatal_lastMenstrualPeriodItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObstetricalCalendar::LastMentrualPeriod");
         prenatal_conceptionDateUi = prenatal_formUi.findChild("conceptionDateDateEdit");
-        prenatal_conceptionDateItem = freemedforms.forms.item("ObstetricalCalendar::ConceptionDate");
+        prenatal_conceptionDateItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObstetricalCalendar::ConceptionDate");
         prenatal_useLmp = prenatal_formUi.findChild("useLmpCheck");
-        prenatal_useLmpItem = freemedforms.forms.item("ObstetricalCalendar::UseLMP::Check");
+        prenatal_useLmpItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObstetricalCalendar::UseLMP::Check");
         prenatal_useDateOfConception = prenatal_formUi.findChild("useDateOfConceptionCheck");
-        prenatal_useDateOfConceptionItem = freemedforms.forms.item("ObstetricalCalendar::UseConceptionDate::Check");        
+        prenatal_useDateOfConceptionItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObstetricalCalendar::UseConceptionDate::Check");        
         prenatal_currentWeeksAmenorrhoeaWeeksValue = prenatal_formUi.findChild("currentWeeksAmenorrhoeaWeeksValueLabel");        
         prenatal_currentWeeksAmenorrhoeaDaysValue = prenatal_formUi.findChild("currentWeeksAmenorrhoeaDaysValueLabel");       
         prenatal_estimatedDueDate = prenatal_formUi.findChild("eddValueLabel");
         prenatal_setDefaults();
         prenatal_setLmpDefault();
         prenatal_computeGestationalAge();
+
+        // BMI
+        prenatal_bmiValueLabel = formUi.findChild("bmiValueLabel");                      
+        prenatal_bmiValueLineEdit = formUi.findChild("bmiValueLineEdit");                
+        //weight = formUi.findChild("weightValue");                               
+        prenatal_weightUnit = formUi.findChild("weightUnit");                            
+        prenatal_weightUnitItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObjectiveGroup::Weight::Value");
+        prenatal_height = formUi.findChild("heightValue");                               
+        prenatal_heightUnit = formUi.findChild("heightUnit");                            
+        prenatal_heightUnitItem = freemedforms.forms.item("Maternity::Obstetrics::Prenatal::ObjectiveGroup::Height::Value");
 
         // fetus connection
         prenatal_fetusNumber['valueChanged(int)'].connect(this, prenatal_hideFetusGroupBox);
@@ -99,6 +113,17 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
         //useDateOfConception['stateChanged(int)'].connect(this, chooseDateToUse);
         //lastMenstrualPeriod['dateChanged(int)'].connect(this, chooseDateToUse);  
         //conceptionDate['dateChanged()'].connect(this, chooseDateToUse);
+
+        //BMI connections
+        prenatal_weight['valueChanged(double)'].connect(this, prenatal_computeBMI);
+        prenatal_height['valueChanged(double)'].connect(this, prenatal_computeBMI);
+        prenatal_weightUnit['currentIndexChanged(int)'].connect(this, prenatal_computeBMI);
+        prenatal_heightUnit['currentIndexChanged(int)'].connect(this, prenatal_computeBMI);
+        // hide bmiValueLineEdit                                                
+        prenatal_bmiValueLineEdit.visible = false;
+
+        // hide prenatal_weightGainLineEdit
+        prenatal_weightGainLineEdit.visible = false;
 
     }
     
@@ -188,16 +213,15 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
 
     function prenatal_computePulsePressure() {                                           
         var prenatal_text = prenatal_syst.value - prenatal_diast.value;                                    
-        prenatal_pulse.setText(text);                                                    
+        prenatal_pulse.setText(prenatal_text);                                                    
     }
 
     // weight gain function
 
     function prenatal_computeWeightGain() {
-        print("prenatal_computeWeightGain()");                                           
         var prenatal_text = prenatal_weight.value - prenatal_weightPrePregnancy.value;                                    
         prenatal_weightGain.setText(prenatal_text);
-        print("prenatal_computeWeightGain() gain = " + prenatal_text);                                                   
+        prenatal_weightGainLineEdit.setText(prenatal_text);
     }
 
     // fatigue score function
@@ -307,6 +331,77 @@ namespace.module('com.freemedforms.obstetrics.prenatal', function (exports, requ
         prenatal_computeGestationalAge();
     }
 
+    function roundToOne(num) {                                                  
+        return +(Math.round(num + "e+1")  + "e-1");                             
+    }
+
+    function weightToKilogram(weightUnit, weightValue) {                        
+        print(weightUnit, weightValue);                                          
+        var ounceToKilogram = 0.028349523125; //International avoirdupois ounce 
+        var poundToKilogram = 0.45359237; //International avoirdupois pound     
+        var gramToKilogram = 0.001;                                             
+        var kilogram = "Kilogram";                                              
+        var gram = "Gram";                                                      
+        var ounce = "Ounce";                                                    
+        var pound = "Pound";                                                    
+        if (~weightUnit.indexOf(gram)) {                                        
+            print("gram to kg", weightValue*gramToKilogram);                    
+            return weightValue*gramToKilogram;                                  
+        } else if (~weightUnit.indexOf(kilogram)) {                             
+            return weightValue;                                                 
+        } else if (~weightUnit.indexOf(ounce)) {                                
+            return weightValue*ounceToKilogram;                                 
+        } else if (~weightUnit.indexOf(pound)) {                                
+            return weightValue*poundToKilogram;                                 
+        }                                                                       
+    }
+
+    function heightToMeter(heightUnit, heightValue) {
+        print(heightUnit, heightValue);                           
+        var centimeterToMeter = 0.01;                                           
+        var inchToMeter = 0.0254;                                               
+        var footToMeter = 0.3048;                                               
+        var centimeter = "Centimeter";                                          
+        var meter = "Meter";                                                    
+        var inch = "Inch";                                                      
+        var foot = "Foot";                                                      
+        if (~heightUnit.indexOf(centimeter)) {                                  
+            return heightValue*centimeterToMeter;                               
+        } else if (~heightUnit.indexOf(meter)) {                                
+            return heightValue;                                                 
+        } else if (~heightUnit.indexOf(inch)) {                                 
+            return heightValue*inchToMeter;                                     
+        } else if (~heightUnit.indexOf(foot)) {                                 
+            return heightValue*footToMeter;                                     
+        }                                                                       
+    }
+
+    function prenatal_computeBMI() {
+        print("Combo has changed");        
+        //var textbmi  = (weight.value) / ((height.value) * (height.value));
+        //bmiValueLabel.setText(textbmi);
+        //bmiValueLineEdit.setText(textbmi);
+        // weight unit kilogram                                                 
+        // height unit meter                                                    
+        var weightUnit = prenatal_weightUnitItem.currentText;                            
+        var weightValue = Number(prenatal_weight.value);                                 
+        var weightKilogram = weightToKilogram(weightUnit, weightValue);             
+                                                                                
+        var heightUnit = prenatal_heightUnitItem.currentText;                            
+        var heightValue = Number(prenatal_height.value);                                 
+        var heightMeter = heightToMeter(heightUnit, heightValue);                   
+                                                                                
+        var bmi  = (weightKilogram) / ((heightMeter) * (heightMeter));          
+        bmi = roundToOne(bmi);                                                  
+        var textbmi = bmi.toString();                                           
+        if (!isNaN(bmi)) {                                                      
+        prenatal_bmiValueLineEdit.setText(textbmi);                                      
+        prenatal_bmiValueLabel.setText(textbmi);                                         
+        } else {                                                                
+        prenatal_bmiValueLineEdit.setText("");                                           
+        prenatal_bmiValueLabel.setText("");                                              
+        }
+    }
 });
 
 namespace.com.freemedforms.obstetrics.prenatal.prenatal_setupUi();
